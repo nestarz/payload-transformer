924b65b849f4fe1f5aa8ecdc8f8011a5a90a2480
Elias Rhouzlane <elias.rhouzlane@gmail.com>
2021-03-08 14:46:17 +0100

    fix: clean send obj

--- api/slack.js
+++ api/slack.js
@@ -1,16 +1,33 @@
 const request = require("request");
 
+function simpleStringify(object) {
+  var simpleObject = {};
+  for (var prop in object) {
+    if (!object.hasOwnProperty(prop)) {
+      continue;
+    }
+    if (typeof object[prop] == "object") {
+      continue;
+    }
+    if (typeof object[prop] == "function") {
+      continue;
+    }
+    simpleObject[prop] = object[prop];
+  }
+  return JSON.stringify(simpleObject); // returns cleaned up JSON
+}
+
 module.exports = (req, res) => {
-  const { fwd, key = "payload" } = req.query;
+  const { fwd } = req.query;
   request.post(
     {
       url: fwd,
-      json: { payload: { text: JSON.stringify(req.body[key]) } },
+      json: { payload: { text: JSON.stringify(req.body) } },
       headers: req.headers,
     },
     (error, response, body) => {
       if (response) res.status(response.statusCode);
-      res.send(error);
+      res.send(simpleStringify(error ? error : body));
     }
   );
 };
