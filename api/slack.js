dc9c9f364c58e440deb938d0c927206d90e67107
Elias Rhouzlane <elias.rhouzlane@gmail.com>
2021-03-08 14:13:22 +0100

    fix: typo

--- api/slack.js
+++ api/slack.js
@@ -1,13 +1,10 @@
 module.exports = (req, res) => {
   const { webhook_fwd } = req.query;
-  req.post({ url: webhook_fwd, headers: req.headers });
-  request.post(
+  req.post(
     {
       url: webhook_fwd,
       json: { payload: { text: JSON.stringify(req.body) } },
-      headers: {
-        "Content-Type": "application/json",
-      },
+      headers: req.headers,
     },
     (error, response, body) =>
       res.status(response.statusCode).send(error ? error : body)
